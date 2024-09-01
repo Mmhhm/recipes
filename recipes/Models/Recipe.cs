746113f9069baa9a496176d0aea3a39ddf81ee0e
moti <mordechayamramy@gmail.com>
2024-09-01 11:12:46 +0300

    added "name" to the model

--- recipes/Models/Recipe.cs
+++ recipes/Models/Recipe.cs
@@ -3,6 +3,7 @@
 	public class Recipe
 	{
 		public int id { get; set; }
+		public string name { get; set; } = string.Empty;
 		public List<string> Ingredients { get; set; } = new List<string>();
 		public List<string> Phases { get; set; } = new List<string>();
 	}
