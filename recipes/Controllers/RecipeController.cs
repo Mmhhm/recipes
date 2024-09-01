0307a2382fac6d1a27546c89b21a24c9d805b0ea
moti <mordechayamramy@gmail.com>
2024-09-01 10:30:44 +0300

    added constructor

--- recipes/Controllers/RecipeController.cs
+++ recipes/Controllers/RecipeController.cs
@@ -1,9 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using recipes.Models;
 
 namespace recipes.Controllers
 {
 	public class RecipeController : Controller
 	{
+		private static List<Recipe> _recipes = new List<Recipe>();
+		private static int _Id = 0;
+		public RecipeController()
+		{
+			if (!_recipes.Any())
+			{
+				_recipes.Add(new Recipe { 
+					id = _Id++, 
+					Ingredients = new List<string> { "sugar", "coffee", "hot Water", "milk" },
+					Phases = new List<string> { "1. add two spoon to the cup", "2. add one spoon of coffee to the cup", "3. boil hot water and fill the cup", "4. add milk and drink!" } });
+			}
+
+		}
+
+
 		public IActionResult Index()
 		{
 			return View();
--- recipes/Models/Recipe.cs
+++ recipes/Models/Recipe.cs
@@ -2,7 +2,7 @@
 {
 	public class Recipe
 	{
-		int id { get; set; }
+		public int id { get; set; }
 		public List<string> Ingredients { get; set; } = new List<string>();
 		public List<string> Phases { get; set; } = new List<string>();
 	}
