70848f6fcbc309badfe505bfba02657749062c61
mkf <mkf@KF>
2024-09-01 10:08:11 +0300

    Created Model

---
+++ recipes/Models/Recipe.cs
@@ -0,0 +1,9 @@
+﻿namespace recipes.Models
+{
+	public class Recipe
+	{
+		int id { get; set; }
+		public List<string> Ingredients { get; set; } = new List<string>();
+		public List<string> Phases { get; set; } = new List<string>();
+	}
+}
