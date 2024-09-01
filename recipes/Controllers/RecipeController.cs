36a1f029911bbfcd9285c690668318755dc4923f
moti <mordechayamramy@gmail.com>
2024-09-01 10:37:41 +0300

    added delete action

--- recipes/Controllers/RecipeController.cs
+++ recipes/Controllers/RecipeController.cs
@@ -11,13 +11,13 @@ namespace recipes.Controllers
 		{
 			if (!_recipes.Any())
 			{
-				_recipes.Add(new Recipe { 
-					id = _Id++, 
+				_recipes.Add(new Recipe
+				{
+					id = _Id++,
 					Ingredients = new List<string> { "sugar", "coffee", "hot Water", "milk" },
-					Phases = new List<string> { "1. add two spoon of sugar to the cup", "2. add one spoon of coffee to the cup", "3. boil hot water and fill the cup", "4. add milk and drink!" } });
-
+					Phases = new List<string> { "1. add two spoon of sugar to the cup", "2. add one spoon of coffee to the cup", "3. boil hot water and fill the cup", "4. add milk and drink!" }
+				});
 			}
-
 		}
 
 
@@ -25,5 +25,19 @@ namespace recipes.Controllers
 		{
 			return View();
 		}
+
+
+		public IActionResult DeleteRecipe(int id)
+		{
+			var myRecipe = _recipes.FirstOrDefault(recipe => recipe.id == id);
+			if (myRecipe == null)
+			{
+				return NotFound();
+			}
+			_recipes.Remove(myRecipe);
+			return RedirectToAction("Index");
+		}
+
 	}
+}
 }
